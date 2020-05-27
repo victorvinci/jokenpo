5a91c910a59cca4b2a3c1dce5b5c41fcd91e76c9
victor vinci <victorvinci@protonmail.com>
2020-05-27 18:23:28 +0100

    added button to rule modal

--- app/components/RulesModal/RulesModal.component.jsx
+++ app/components/RulesModal/RulesModal.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from '../../components';
 
 export default class RulesModal extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class RulesModal extends Component {
   render() { 
     return ( 
       <section>
+        <Button />
         <h3>Rules</h3>
       </section>
     );
