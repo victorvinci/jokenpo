68305616a30c61289425352aef9e5bfb5fca307c
victor vinci <victorvinci@protonmail.com>
2020-05-27 18:23:13 +0100

    added basic styling "placeholder" for SelectOption

--- app/components/SelectOption/SelectOption.components.jsx
+++ app/components/SelectOption/SelectOption.components.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
+import './_SelectOption.scss';
 export default class SelectOption extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
-  
+
   render() { 
     return ( 
-      <section>
-        <option value="rock">Rock</option>
-        <option value="paper">Paper</option>
-        <option value="scissor">Scissor</option>
+      <section className="select">
+        <option className="select-rock" value="rock">Rock</option>
+        <option className="select-paper" value="paper">Paper</option>
+        <option className="select-scissor" value="scissor">Scissor</option>
       </section>
     );
   }
