b4667a8cd7459e861cf75d463b8851bcab074df9
victor vinci <victorvinci@protonmail.com>
2020-05-27 17:31:24 +0100

    added sass loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,11 @@ module.exports = {
       {
         test : /\.css$/, 
         use:['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader','sass-loader'],
+      },
     ]
   },
   mode:'development',
